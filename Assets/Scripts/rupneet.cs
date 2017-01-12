523bfebe1980ae11230849fde54e4ce944824a0b
Rupneet Atwal <rupneeta@sfu.ca>
2017-01-12 13:48:46 -0800

    Test

---
+++ Assets/Scripts/rupneet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rupneet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
